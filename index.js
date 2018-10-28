42183dd11fb777b54aa10a3bf65cbc296d0926f3
Justus1994 <buybypenny@googlemail.com>
2018-10-28 16:07:57 +0100

    update index.js
    send name + unit gp

--- index.js
+++ index.js
@@ -7,11 +7,27 @@ const swapi = new ApiSwgohHelp({
   "password" : botconfig.password
 });
 
+var data;
+
 const bot = new Discord.Client();
+
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online`);
   bot.user.setActivity("Swgoh");
+
+  console.log("collect data");
+
+  let payload = {
+    "allycodes" : botconfig.members,
+    "language" : "eng_us"
+  }
+
+  this.data =  await swapi.fetchUnits(payload);
+
 });
+
+
+
 
 bot.on("message", async message => {
   // This event will run on every single message received, from any channel.
@@ -22,6 +38,7 @@ bot.on("message", async message => {
 
   const args = message.content.slice(botconfig.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
+
   if(command === "ping") {
     // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
     // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
@@ -30,48 +47,42 @@ bot.on("message", async message => {
   }
 
 
-  if(command === "units"){
+  if(command === "whtb"){
+    var foundUnit = false;
     let [arg1] = args;
 
     try {
-        console.log('++ Testing units ++\n');
-        let payload = {
-            "allycodes": args,
-            "language": "eng_us"
-          };
+        for(var i in this.data){
+         if (i === arg1.toUpperCase()){
+           foundUnit = true;
+           var key = i;
+            console.log(i);
 
-          var data = await swapi.fetchUnits(payload);
+            var test = [];
+            var test2 = [];
 
-          var units = 0;
+            var msg = "";
+            for(var k = 0; k <= botconfig.MemberCount; k++){
+              test.push(this.data[key][k].player);
+              test2.push(this.data[key][k].gp);
+              msg += this.data[i][k].player + " : " +  this.data[i][k].gp + "\n";
+            }
 
-
-        for(var i in data){
-         if (i === "EMBO"){
-           var embo = JSON.stringify(data[i])
-             const m = await message.channel.send(embo);
+            const m = await message.channel.send(i + ": \n" + msg);
          }
-        }
+       }
 
+       if(foundUnit === false){
+         const m = await message.channel.send("no Unit with name : " + arg1);
+         foundUnit = false;
+       }
     } catch(e) {
         throw e;
     }
   }
 
-
-  if(command === "whtb"){
-    let[arg1, arg2] = args;
-    if(arg1 === "help")
-    {
-    const m = await message.channel.send("Example command: !whtb Embo");
-    }
-    else if(arg1)
-    {
-    arg1 = arg1.charAt(0).toUpperCase() + arg1.slice(1);
-    const m = await message.channel.send("Niko has the best " +   `${arg1}`);
-    }
-    else{
-    const m = await message.channel.send("Bad command use !whtb help for an example");
-    }
-  }
 });
+
+
+
 bot.login(botconfig.token);
